0b78bf79ba6466a49e8b3123cf4e46f59c40ab4d
hadrianuspage <wibowos27@gmail.com>
2023-03-30 16:35:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let buttonLink = document.getElementById("link");
+let link = buttonLink.getAttributeNode("href").value;
+console.log(link)
+
+
+buttonLink.addEventListener("click", function(){
+  buttonLink.addEventListener("click", function(){
+  buttonLink.setAttribute("href", "url-asli-disini")
+  buttonLink.removeAttribute("target")
+})
+})
+
 var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
 var menu = document.getElementsByClassName('menu')[0];
 
