9d6333b7c933aed380d8031ff3ce56cba2b678fa
hadrianuspage <wibowos27@gmail.com>
2023-03-30 17:13:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-function changeLink() {
-    var link = document.getElementById("mylink");
-
-    window.open( 
-      link.href, 
-      '_blank' 
-    ); 
-
-    link.innerHTML = "mediafire"; 
-    link.setAttribute('href', "https://www.mediafire.com/file/jwcnsl9m1mtk03u/Minecraft_base.apk/file"); 
-
-    return false; 
-}
-
-
 var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
 var menu = document.getElementsByClassName('menu')[0];
 
