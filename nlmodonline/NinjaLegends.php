2dd504e3c05d87b8ad530871ded114ecc5dc3039
hadrianuspage <wibowos27@gmail.com>
2024-03-14 16:17:08 +0700

    Create NinjaLegends.php

---
+++ nlmodonline/NinjaLegends.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Ninja Legends Web Version</title>
+</head>
+<body>
+<div id="game-container" class="flash-game">
+<object type="application/x-shockwave-flash" data="NinjaLegends.swf" width="960" height="540">
+<param name="movie" value="NinjaLegends.swf" />
+</object>
+</div>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v84a3a4012de94ce1a686ba8c167c359c1696973893317" integrity="sha512-euoFGowhlaLqXsPWQ48qSkBSCFs3DPRyiwVu3FjR96cMPx+Fr+gpWRhIafcHwqwCqWS42RZhIudOvEI+Ckf6MA==" data-cf-beacon='{"rayId":"8642be1a6a6ad6ae","version":"2024.2.4","r":1,"token":"b25807845f9e4b418c282d6d49d87e5a","b":1}' crossorigin="anonymous"></script>
+</body>
+</html>
